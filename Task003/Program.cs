dc44b7329d40a181ab335b4db1af8ecb14c73423
Andrei_Kadyrov <kadyrov_andrei@mail.ru>
2022-08-16 14:29:09 +0300

    add Task 003 Even or Odd number

---
+++ Task003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.
+
+Console.Clear();
+
+Console.WriteLine("Введите целое число: ");
+int a  = Convert.ToInt32(Console.ReadLine());
+
+if(a %2 == 0)
+{
+    Console.WriteLine("Это число является четным");
+}
+else
+{
+    Console.WriteLine("Это число является нечетным");
+}
