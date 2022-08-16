51a26d7c6a71ad0ce3cd556f758e1324ea14b41f
Andrei_Kadyrov <kadyrov_andrei@mail.ru>
2022-08-16 14:39:55 +0300

    add Task 010 TableCube

---
+++ Task010/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Напишите программу, которая принимает на вход число (N) и выдаёт      // таблицу кубов чисел от 1 до N.
+        //3 -> 1, 8, 27
+        //5 -> 1, 8, 27, 64, 125
+
+        Console.Write("Введите целое число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int tableCube = 1;
+
+        if (number < 1) Console.WriteLine("Перезапустите программу и введите целое число больше или равное 1");
+
+        while (tableCube <= number)
+        {
+            Console.Write(Math.Pow(tableCube, 3) + "; ");
+            tableCube++;
+        }
+    }
+}
