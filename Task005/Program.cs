40b966f0c6b1d26c21758424e701e761ffb43902
Andrei_Kadyrov <kadyrov_andrei@mail.ru>
2022-08-16 14:32:57 +0300

    add Task 005 Show second digit treeDigitNumber

---
+++ Task005/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.Write("Введите трёхзначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        
+        string TwoDigitNumber = Convert.ToString(number); 
+       
+        Console.WriteLine("Вторая цифра этого числа: "+TwoDigitNumber[1]);
+    }
+}
+//Convert.ToString(int value) - Преобразует значение указанного
+//32-разрядного целого числа со знаком в его эквивалентное строковое представление.
+
+
