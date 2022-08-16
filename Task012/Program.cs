ffe32d41be8e70d92cd911ef83ae1a4ff78b6c08
Andrei_Kadyrov <kadyrov_andrei@mail.ru>
2022-08-16 14:47:02 +0300

    add Task 012 SumDigitNumber

---
+++ Task012/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program {
+  public static void Main (string[] args) {
+    //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+    //452 -> 11
+    //82 -> 10
+    //9012 -> 12
+    
+    Console.Clear();
+    Console.Write("Введите целое число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int sum = 0;
+    
+    while(number > 0)
+    {
+      int digit = number % 10;
+      sum = sum + digit;
+      number = number/10;
+    } 
+    
+    Console.Write($"Сумма цифр в числе: {sum}");
+    
+  }
+}
