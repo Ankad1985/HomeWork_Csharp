be1e9813b9b590768e788f25a62e4d7a1c919c2d
Andrei_Kadyrov <kadyrov_andrei@mail.ru>
2022-08-16 14:39:12 +0300

    add Task 009 Distance 3D

---
+++ Task009/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+        //A (3,6,8); B (2,1,-7), -> 15.84
+        //A (7,-5, 0); B (1,-1,9) -> 11.53
+        Console.Clear();
+        //Ввод координат точки А
+        Console.WriteLine("Введите координату х для точки А: ");
+        int x1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите координату y для точки А: ");
+        int y1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите координату z для точки А: ");
+        int z1 = Convert.ToInt32(Console.ReadLine());
+
+        //Ввод координат точки B
+        Console.WriteLine("Введите координату х для точки B: ");
+        int x2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите координату y для точки B: ");
+        int y2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите координату z для точки B: ");
+        int z2 = Convert.ToInt32(Console.ReadLine());
+
+        Double Segment = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+        //Округление до двух знаков после запятой
+        Double Distance = Math.Round(Segment, 2);
+
+        Console.WriteLine("Расстояние между точками в 3D: " + Distance);
+
+
+
+    }
+
+}
+
