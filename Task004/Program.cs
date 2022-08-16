dc51ea4f4e2aab21b1fc68247fd626b806fe7e8a
Andrei_Kadyrov <kadyrov_andrei@mail.ru>
2022-08-16 14:30:36 +0300

    add Task 004 Even 1 to N

---
+++ Task004/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear(); // Очистка консоли перед запуском программы
+
+Console.Write("Введите целое число: ");
+int number  = Convert.ToInt32(Console.ReadLine()); // Ввод числа пользователем
+
+int evenNumber = 2; // Чётные числа
+
+while(evenNumber <= number)
+{
+    Console.WriteLine(evenNumber);
+    evenNumber = evenNumber + 2;
+}
