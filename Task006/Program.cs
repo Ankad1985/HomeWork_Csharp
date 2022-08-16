6fcfc584b26c938b8ddc5305255ce2d287b02361
Andrei_Kadyrov <kadyrov_andrei@mail.ru>
2022-08-16 14:35:37 +0300

    add Task 006 Report treeDigitNumber

---
+++ Task006/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+        Console.Clear();
+    
+        Console.Write("Введите трёхзначное число: ");
+        int number  = Convert.ToInt32(Console.ReadLine());
+        string Digit = Convert.ToString(number);
+    
+        if(Digit.Length >2) 
+        {
+            Console.WriteLine("Третья цифра введённого числа:"+Digit[2]);
+        }  
+        else
+        {
+        Console.WriteLine("Третьей цифры нет.");
+        } 
+    }
+}
