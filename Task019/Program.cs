60a05ca68c172fbeeae14bbb921b9d60b75656cc
Andrei_Kadyrov <kadyrov_andrei@mail.ru>
2022-08-28 12:55:23 +0300

    add Task 019 Two Array Fill Random Real Numbers

---
+++ Task019/Program.cs
@@ -0,0 +1,54 @@
+﻿/*Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.   
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.Write("\nВведите количество строк в двумерном массиве: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("\nВведите количество столбцов в двумерном массиве: ");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        double[,] numbers = new double[rows, columns];
+
+        //Метод заполнения двумерного массива случайными числами
+        void FillArray(double[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+                }
+            }
+
+        }
+        //---------------------------------------------------
+
+        //Метод вывода двумерного массива
+        void PrintArray(double[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        //---------------------------------------------------
+        FillArray(numbers);
+        PrintArray(numbers);
+
+
+    }
+}
