76e85820b6e5ddf0ab1414f43cd4b15ffb22caf2
Andrei_Kadyrov <kadyrov_andrei@mail.ru>
2022-08-28 13:09:05 +0300

    add Task 021 Two Array Average Columns

---
+++ Task021/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        int[,] numbers = {{1,4,7,2},
+                          {5,9,2,3},
+                          {8,4,2,4}};
+        Console.WriteLine($"\nСреднее арифметическое каждого столбца: ");
+
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            double sumColumn = 0;
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                sumColumn = sumColumn + numbers[i, j];
+            }
+            double averageColumn = Math.Round(sumColumn / numbers.GetLength(0), 2);
+            Console.WriteLine($"\nСтолбец №{j + 1} = {averageColumn}");
+        }
+
+    }
+}
